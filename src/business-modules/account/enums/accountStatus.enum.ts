3122b9807a6f6665116f809006f15d0dcd3d93af
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-07-31 01:32:37 -0300

    preparing folders and project structure

---
+++ src/business-modules/account/enums/accountStatus.enum.ts
@@ -0,0 +1,6 @@
+export enum AccountStatus {
+  ACTIVE = 1,
+  DEACTIVATED = 2,
+  DONE = 3,
+  CANCELED = 4,
+}
---
+++ src/business-modules/account/enums/accountType.enum.ts
@@ -0,0 +1,5 @@
+export enum AccountType {
+  CHECKING_ACCOUNT = 1, // Conta Corrente
+  SAVINGS_ACCOUNT = 2, // Conta Poupança
+  PAYROLL_ACCOUNT = 3, // Conta Salário
+}
