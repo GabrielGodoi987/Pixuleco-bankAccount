59279a400448ce3c952c392ac769efa5e92c837e
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-09-02 16:35:17 -0300

    initiation of unit tests

---
+++ test/modules/customers/e2e/user.spec.ts
@@ -0,0 +1,9 @@
+describe('', () => {
+  afterAll(() => {});
+
+  describe('GET /user', () => {
+    it('should return one user', () => {});
+    it('Should return an not found error', () => {});
+    it('Should return an not authorize exception', () => {});
+  });
+});
